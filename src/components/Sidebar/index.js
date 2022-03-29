d67444dc7dcff441ae5bb08c5afc766ebee64a74
Bola Gadalla <bolagadalla777@gmail.com>
2022-03-29 12:17:15 -0400

    Added my logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import Duck_File from '../../assets/images/Duck_File.png';
+import LogoSubtitle from '../../assets/images/Bola_Gadalla.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -9,7 +9,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Sidebar = () => (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
-      <img src={LogoS} alt='logo'/>
+      <img src={Duck_File} alt='logo'/>
       <img className='sub-logo' src={LogoSubtitle} alt='Bola Gadalla'/>
     </Link>
 
