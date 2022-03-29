cd55204ea0e39bbbc87fc6bd689f404c794a3f70
Bola Gadalla <bolagadalla777@gmail.com>
2022-03-29 14:44:17 -0400

    Added some content on the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,18 @@
-const Home = () => ('Hello');
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>Hi, <br/> I'm Bola
+        <br/>
+        Software Engineer
+        </h1>
+        <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+      </div>
+    </div>
+  )
+}
 
 export default Home;
