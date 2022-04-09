a113aacbde2c4d2e767dbcab845cc502fe3e168f
Bola Gadalla <bolagadalla777@gmail.com>
2022-04-09 18:35:24 -0400

    Added a contact me page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import emailjs from '@emailjs/browser'
+
+
+const emailJSCredentials = require("../../assets/auth/credentials.json")
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
   useEffect(() => {
     return setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
   
+  const sendEmail = (e) => {
+    // emailjs.init('FtFIZN5nZXVECCMFI')
+    e.preventDefault()
+    emailjs.sendForm(
+      'gmail',
+      emailJSCredentials.emailjs.templateID,
+      refForm.current,
+      emailJSCredentials.emailjs.userID
+    ).then((r) => {
+      alert('Message Sent Successfully!')
+      console.log(r.text)
+      window.location.reload(false)
+    }, (e) => {
+      console.log(e)
+      alert('Couldn\'t Send Message, Please Try Again')
+    })
+  }
+
   return (
     <>
     <div className='container contact-page'>
@@ -18,6 +41,33 @@ const Contact = () => {
         <h1>
           <AnimatedLetters letterClass={letterClass} strArray={[...'Contact me']}/>
         </h1>
+        <p>
+          I am interest in working with a cool company with cool projects.
+        </p>
+
+        <div className='contact-form'>
+          <form ref={refForm} onSubmit={sendEmail}>
+            {/* <div className='g-recaptcha' data-sitekey='6LedDl8fAAAAACVhyGg_EZADvmTjxEwAz8eFcGPQ'></div> */}
+            <ul>
+              <li className='half'>
+                <input type='text' name='name' placeholder='Name' required />
+              </li>
+              <li className='half'>
+                <input type='email' name='email' placeholder='Email' required />
+              </li>
+              <li>
+                <input placeholder='Subject' type='text' name='subject' required/>
+              </li>
+              <li>
+                <textarea placeholder='Message' name='message' required></textarea>
+              </li>
+              <li>
+                <input type='submit' className='flat-button' value='Send'/>
+              </li>
+            </ul>
+          </form>
+        </div>
+
       </div>
     </div>
     <Loader type='pacman'/>
